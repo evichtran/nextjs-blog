a9a72aa84e884643852204a9eb7c4d8a396c7708
Evich Tran <leohnhat2012@gmail.com>
2024-05-28 17:23:52 +0700

    Change somethings #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,51 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1>Hello ae đến với EvichTran!</h1>
-      <p>t là 1 ông dev quèn, 1 hacker và wibu</p>
+      <h1>NinjaDev Team</h1>
+      <div className="header">
+        <div className="header__inner">
+          <div className="logo">
+            <img src="logo.jpg" />
+          </div>
+          <ul className="menu">
+            <li className="menu__item">
+              <a href="" className="menu__link">
+                Home
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="" className="menu__link">
+                About
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="" className="menu__link">
+                Blogs
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="" className="menu__link">
+                Products
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="" className="menu__link">
+                Pricing
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="" className="menu__link">
+                CV
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="" className="menu__link">
+                Contact Admin
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
