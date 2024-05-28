c63ffbec9c22cd91dd06ce8f0635a3b4e2769741
Evich Tran <leohnhat2012@gmail.com>
2024-05-28 20:19:33 +0700

    Change somethings #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,83 @@
 export default function Home() {
   return (
     <div>
-      <h1>NinjaDev Team</h1>
-      <div className="header">
-        <div className="header__inner">
-          <div className="logo">
-            <img src="logo.jpg" />
-          </div>
-          <ul className="menu">
-            <li className="menu__item">
-              <a href="" className="menu__link">
-                Home
-              </a>
+      {/* <h1>NinjaDev Team</h1> */}
+      <ul className="nav">
+        <li>
+          <a href="#">Homepage</a>
+          <ul className="sub-menu">
+            <li>
+              <a href="#">Home</a>
             </li>
-            <li className="menu__item">
-              <a href="" className="menu__link">
-                About
-              </a>
+            <li>
+              <a href="#">About</a>
             </li>
-            <li className="menu__item">
-              <a href="" className="menu__link">
-                Blogs
-              </a>
+            <li>
+              <a href="#">History</a>
             </li>
-            <li className="menu__item">
-              <a href="" className="menu__link">
-                Products
-              </a>
-            </li>
-            <li className="menu__item">
-              <a href="" className="menu__link">
-                Pricing
-              </a>
-            </li>
-            <li className="menu__item">
-              <a href="" className="menu__link">
-                CV
-              </a>
-            </li>
-            <li className="menu__item">
-              <a href="" className="menu__link">
-                Contact Admin
-              </a>
+            <li>
+              <a href="#">Collaborate</a>
             </li>
           </ul>
-        </div>
-      </div>
+        </li>
+        <li>
+          <a href="#">Blogs</a>
+          <ul className="sub-menu">
+            <li>
+              <a href="#">HTML & CSS</a>
+            </li>
+            <li>
+              <a href="#">Javascript</a>
+            </li>
+            <li>
+              <a href="#">Typescript</a>
+            </li>
+            <li>
+              <a href="#">Reactjs</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Course</a>
+          <ul className="sub-menu">
+            <li>
+              <a href="#">Cơ bản</a>
+              <ul className="sub-menu">
+                <li>
+                  <a href="#">HTML</a>
+                </li>
+                <li>
+                  <a href="#">CSS</a>
+                </li>
+                <li>
+                  <a href="#">JavaScript</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Nâng cao</a>
+              <ul className="sub-menu">
+                <li>
+                  <a href="#">Nextjs(Frontend)</a>
+                </li>
+                <li>
+                  <a href="#">Nestjs(Backend)</a>
+                </li>
+                <li>
+                  <a href="#">Deloy web app</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Create CV</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <button className="button">Register</button>
     </div>
   );
 }
